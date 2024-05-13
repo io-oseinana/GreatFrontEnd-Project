09ac29e2523d50c2d5e80368be63192c811b4009
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2024-05-13 17:26:06 +0000

    Add mobile menu function

--- js/index.js
+++ js/index.js
@@ -1,2 +1,28 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const header = document.querySelector('header');
+const menuBtn = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.close');
+const menuContainer = document.querySelector('.nav_btn-toggle-menu');
+
+menuBtn.addEventListener('click', () => {
+  const nav = document.querySelector('.nav_mobile');
+  nav.classList.toggle('active');
+  header.classList.toggle('active');
+  menuContainer.classList.add('active');
+
+  if (menuContainer.classList.contains('active')) {
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'block';
+  } else {
+    menuBtn.style.display = 'block';
+    closeBtn.style.display = 'none';
+  }
+});
+
+closeBtn.addEventListener('click', () => {
+  const nav = document.querySelector('.nav_mobile');
+  nav.classList.remove('active');
+  header.classList.remove('active');
+  menuContainer.classList.remove('active');
+  menuBtn.style.display = 'block';
+  closeBtn.style.display = 'none';
+});
