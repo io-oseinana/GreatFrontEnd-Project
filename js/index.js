d398a450fe0f257db4fc881f907b7f379148aae7
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2024-05-16 04:39:49 +0000

    Add comments to code

--- js/index.js
+++ js/index.js
@@ -2,10 +2,14 @@ const emailInput = document.querySelector('.subscribe-input');
 const submitButton = document.querySelector('.subscribe-btn');
 const hintText = document.querySelector('.hint-text');
 
+// Regex for checking whether the email is valid or not
+
 const validEmail = (email) => {
   const reg = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
   return reg.test(String(email).toLowerCase());
 };
+
+//  Notification function for displaying success or failure message
 
 const notification = document.querySelector('.toast-notification');
 const messageText = document.querySelector('.message');
@@ -40,15 +44,18 @@ const notifyError = (badgeText, messageText) => {
   }, 3000);
 };
 
+// Event listeners for the submit button and email input field
+
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   const hintMsg1 = 'Please enter valid email address.';
   const hintMsg2 = 'Email address is required.';
 
-  if (emailInput.value === '') {
+  if (emailInput.value === '') { // checking for empty string means that we should not send email if the input is empty
     hintText.textContent = hintMsg2;
     hintText.style.opacity = '1';
-  } else if (!validEmail(emailInput.value)) {
+    emailInput.style.boxShadow = ' 0px 0px 0px 4px rgba(217, 45, 32, 0.12), 0px 0px 0px 1px rgba(217, 45, 32, 1)'; // changing boxShadow if input field to focus user attention
+  } else if (!validEmail(emailInput.value)) { // using Regex to check email input  and validate it
     hintText.textContent = hintMsg1;
     hintText.style.opacity = '1';
   } else {
@@ -56,9 +63,15 @@ submitButton.addEventListener('click', (e) => {
     hintText.style.opacity = '0';
     emailInput.value = '';
 
+    // Send email to the user with the email address
+
+    // The URL from which to fetch the email
+    const url = new URL('https://mail.google.com/mail/');
+
     const sendEmail = async (email) => {
+      email = url.searchParams.get('email');
       try {
-        const response = await fetch('https://mail.google.com/mail/oseinana ', {
+        const response = await fetch(url, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -79,6 +92,8 @@ submitButton.addEventListener('click', (e) => {
     sendEmail(emailInput.value);
   }
 });
+
+// Remove the hint text when the user starts typing
 
 emailInput.addEventListener('focus', () => {
   hintText.textContent = '';
