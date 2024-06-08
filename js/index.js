e63f5a1224702ecb5eabb1dca353d8dfd7281a6e
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2024-06-08 14:30:03 +0000

    Add conditional stetement

--- js/index.js
+++ js/index.js
@@ -3,4 +3,10 @@ const btnShow = document.querySelector('#btn-show');
 
 btnShow.addEventListener('click', () => {
     sectionRigtht.classList.toggle('active');
+
+    if (sectionRigtht.classList.contains('active')) {
+        btnShow.innerText= 'Click to hide';
+    } else {
+        btnShow.innerText= 'Show other section';
+    }
     });
