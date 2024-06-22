3989750c1df32fc26a608af27dc1f1c649a4d4d8
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2024-06-22 12:57:35 +0000

    Fetch data from api and render it

--- js/index.js
+++ js/index.js
@@ -1,2 +1,38 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const statsContainer = document.querySelector('.stats-content');
+fetch(
+  "https://www.greatfrontend.com/api/projects/challenges/statistics-metrics"
+) 
+.then((response) => response.json())
+.then(data => {
+    data.data.forEach(item => {
+
+      const metricsDiv = document.createElement('div');
+      metricsDiv.classList.add('metrics');
+
+      const textPair = document.createElement('div')
+      textPair.classList.add('text-pair');
+      
+      const metricValue = document.createElement('p');
+      metricValue.classList.add('value');
+      metricValue.textContent = item.value.toLocaleString();
+
+      const metricName = document.createElement('p');
+      metricName.classList.add('metrics');
+        metricName.textContent = capitalizeFirstLetter(item.metric);
+
+        textPair.appendChild(metricValue);
+        textPair.appendChild(metricName);
+
+        metricsDiv.appendChild(textPair);
+
+        statsContainer.appendChild(metricsDiv)
+    })
+        
+})
+.catch(error => {
+  console.error('Error fetching data:', error);
+});
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
