21e78d974fc1069e20c14ea5e7064b9d677ca0f2
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2024-06-24 09:55:43 +0000

    Add a loading component

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 const statsContainer = document.querySelector('.stats-content');
+const loading = document.querySelector('.loading-container');
+
+loading.style.display = 'block';
 fetch(
   "https://www.greatfrontend.com/api/projects/challenges/statistics-metrics"
 ) 
 .then((response) => response.json())
 .then(data => {
+  loading.style.display = 'none';
+
     data.data.forEach(item => {
 
       const metricsDiv = document.createElement('div');
@@ -13,12 +18,12 @@ fetch(
       textPair.classList.add('text-pair');
       
       const metricValue = document.createElement('p');
-      metricValue.classList.add('value');
+      metricValue.classList.add('metrics-value');
       metricValue.textContent = item.value.toLocaleString();
 
       const metricName = document.createElement('p');
-      metricName.classList.add('metrics');
-        metricName.textContent = capitalizeFirstLetter(item.metric);
+      metricName.classList.add('metrics-name');
+        metricName.textContent = formatMetricName(item.metric);
 
         textPair.appendChild(metricValue);
         textPair.appendChild(metricName);
@@ -31,8 +36,21 @@ fetch(
 })
 .catch(error => {
   console.error('Error fetching data:', error);
+  loading.style.display = 'none';
 });
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+
+function formatMetricName(metric) {
+  let formattedMetric = metric
+    .toLowerCase()
+    .replace(/_/g, ' ')
+    .replace(/downloads/, 'Downloads')
+    .replace(/paid users/, 'Paid users')
+    .replace(/library images/, 'Images in library');
+
+  // Capitalize only the first letter of the first word
+  formattedMetric = formattedMetric.charAt(0).toUpperCase() + formattedMetric.slice(1);
+
+  return formattedMetric;
 }
+
