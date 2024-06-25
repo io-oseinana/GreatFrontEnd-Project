4845f837de94d28efc19b16278cf9cb832352c6b
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2024-06-25 11:05:18 +0000

    Format code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-const statsContainer = document.querySelector('.stats-content');
-const loading = document.querySelector('.loading-container');
+const statsContainer = document.querySelector(".stats-content");
+const loading = document.querySelector(".loading-container");
 
-loading.style.display = 'block';
+loading.style.display = "block";
 
 async function fetchData() {
   try {
@@ -10,56 +10,52 @@ async function fetchData() {
     );
     const data = await response.json();
 
-    loading.style.display = 'none';
+    loading.style.display = "none";
 
-    data.data.forEach(item => {
+    data.data.forEach((item) => {
+      const metricsDiv = document.createElement("div");
+      metricsDiv.classList.add("metrics");
 
-      const metricsDiv = document.createElement('div');
-      metricsDiv.classList.add('metrics');
+      const textPair = document.createElement("div");
+      textPair.classList.add("text-pair");
 
-      const textPair = document.createElement('div')
-      textPair.classList.add('text-pair');
-      
-      const metricValue = document.createElement('p');
-      metricValue.classList.add('metrics-value');
+      const metricValue = document.createElement("p");
+      metricValue.classList.add("metrics-value");
       metricValue.textContent = item.value.toLocaleString();
 
-      const metricName = document.createElement('p');
-      metricName.classList.add('metrics-name');
-        metricName.textContent = formatMetricName(item.metric);
+      const metricName = document.createElement("p");
+      metricName.classList.add("metrics-name");
+      metricName.textContent = formatMetricName(item.metric);
 
-        textPair.appendChild(metricValue);
-        textPair.appendChild(metricName);
+      textPair.appendChild(metricValue);
+      textPair.appendChild(metricName);
 
-        metricsDiv.appendChild(textPair);
+      metricsDiv.appendChild(textPair);
 
-        statsContainer.appendChild(metricsDiv)
-    })
+      statsContainer.appendChild(metricsDiv);
+    });
 
     function formatMetricName(metric) {
       let formattedMetric = metric
         .toLowerCase()
-        .replace(/_/g, ' ')
-        .replace(/downloads/, 'Downloads')
-        .replace(/paid users/, 'Paid users')
-        .replace(/library images/, 'Images in library');
-    
-      formattedMetric = formattedMetric.charAt(0).toUpperCase() + formattedMetric.slice(1);
-    
+        .replace(/_/g, " ")
+        .replace(/downloads/, "Downloads")
+        .replace(/paid users/, "Paid users")
+        .replace(/library images/, "Images in library");
+
+      formattedMetric =
+        formattedMetric.charAt(0).toUpperCase() + formattedMetric.slice(1);
+
       return formattedMetric;
     }
-    
-  
   } catch (error) {
-    const errorDiv = document.createElement('div');
-    errorDiv.classList.add('error-message');
-    errorDiv.textContent = `Error: Error while fetching for data,  ${error.message}`
-    loading.style.display = 'none';
+    const errorDiv = document.createElement("div");
+    errorDiv.classList.add("error-message");
+    errorDiv.textContent = `Error: Error while fetching for data,  ${error.message}`;
+    loading.style.display = "none";
 
     statsContainer.appendChild(errorDiv);
-
   }
-
 }
 
 fetchData();
