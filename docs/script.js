ee611c0e9502d90ee23375974786138800142de7
Osei-Bonsu Isaac <105572944+io-oseinana@users.noreply.github.com>
2024-08-04 01:59:19 +0000

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,7 @@ const projectContainer = document.querySelector('.project-container');
 const searchInput = document.querySelector('#search');   
 async function getProject() {
     try {
-        const res = await fetch('./projects.json');
+        const res = await fetch('projects.json');
         const data = await res.json();
         console.log(data);
 
