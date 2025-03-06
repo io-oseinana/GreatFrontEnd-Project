70fdbad722959b867623491ada4ff3a47f7987c3
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2025-03-06 09:20:16 +0000

    refactor

--- docs/Foundational_skills/contact-section/tailwind.config.js
+++ docs/Foundational_skills/contact-section/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '1xl': '0px 4px 6px -2px rgb(0 0 0 / 0.05), 0px 10px 15px -3px rgb(0 0 0 / 0.10)',
+        'icon': '0px 1px 2px 0 rgb(0 0 0 / 0.06), 0px 1px 3px 0 rgb(0 0 0 / 0.10)',
+      }
+    },
   },
   plugins: [],
 }
