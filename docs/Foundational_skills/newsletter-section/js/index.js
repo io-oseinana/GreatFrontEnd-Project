18e31e0f49c2172f57e3de9a0cc58a597728de3c
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2025-03-12 14:20:52 +0000

    Add a validation for email field

--- docs/Foundational_skills/newsletter-section/js/index.js
+++ docs/Foundational_skills/newsletter-section/js/index.js
@@ -1,2 +1,22 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const emailInput = document.querySelector('#email-input');
+const emailError = document.querySelector('#email-error');
+const newsletterForm = document.querySelector('#newsletter-form');
+
+const toast = document.querySelector('#toast');
+
+emailInput.addEventListener('input', function () {
+    const {validity} = this;
+    emailError.style.display = validity.valid ? 'none' : 'block';
+    emailInput.classList.toggle('input__field--error', !validity.valid);
+
+    if (validity.typeMismatch) {
+        emailError.textContent = 'Please enter a valid email address.';
+    } else if (validity.valueMissing) {
+        emailError.textContent = 'Email address is required.';
+    }
+});
+
+
+// newsletterForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+// }
