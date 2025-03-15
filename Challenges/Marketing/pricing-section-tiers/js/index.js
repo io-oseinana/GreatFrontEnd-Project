8bec1924f962365020852b2cd0527cd7e9a3bf7b
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2025-03-15 15:40:51 +0000

    Fix errors projects not displaying

--- Challenges/Marketing/pricing-section-tiers/js/index.js
+++ Challenges/Marketing/pricing-section-tiers/js/index.js
@@ -3,201 +3,201 @@
   This is because, getting the  json data from directory will not work on the local server. 
 */
 
-fetch("/pricing-section-tiers/data/pricingData.json")
-  .then((response) =>
-    response.json().then((pricingData) => {
-      const subscriptionContainer =
-        document.querySelector(".subscription-tier");
-      let currentPricing = "monthly";
+fetch("/data/pricingData.json")
+    .then((response) =>
+        response.json().then((pricingData) => {
+            const subscriptionContainer =
+                document.querySelector(".subscription-tier");
+            let currentPricing = "monthly";
 
-      const createCards = (plan) => {
-        const pricingCard = document.createElement("div");
-        pricingCard.className = `pricing-card ${plan.popular ? "popular" : ""}`;
+            const createCards = (plan) => {
+                const pricingCard = document.createElement("div");
+                pricingCard.className = `pricing-card ${plan.popular ? "popular" : ""}`;
 
-        const highlight = document.createElement("div");
-        highlight.className = "highlight";
+                const highlight = document.createElement("div");
+                highlight.className = "highlight";
 
-        const highlightText = document.createElement("p");
-        highlightText.textContent = plan.popular ? "Most Popular" : "";
+                const highlightText = document.createElement("p");
+                highlightText.textContent = plan.popular ? "Most Popular" : "";
 
-        highlight.appendChild(highlightText);
+                highlight.appendChild(highlightText);
 
-        // text pair container one
-        const textPairOne = document.createElement("div");
-        textPairOne.className = "text-pair";
+                // text pair container one
+                const textPairOne = document.createElement("div");
+                textPairOne.className = "text-pair";
 
-        const planTitle = document.createElement("h2");
-        planTitle.textContent = plan.plan;
+                const planTitle = document.createElement("h2");
+                planTitle.textContent = plan.plan;
 
-        const planDescription = document.createElement("p");
-        planDescription.textContent = plan.description;
+                const planDescription = document.createElement("p");
+                planDescription.textContent = plan.description;
 
-        // append children
-        textPairOne.appendChild(planTitle);
-        textPairOne.appendChild(planDescription);
+                // append children
+                textPairOne.appendChild(planTitle);
+                textPairOne.appendChild(planDescription);
 
-        // text pair container two
-        const textPairTwo = document.createElement("div");
-        textPairTwo.className = "text-pair";
+                // text pair container two
+                const textPairTwo = document.createElement("div");
+                textPairTwo.className = "text-pair";
 
-        const price = document.createElement("div");
-        price.className = "price";
+                const price = document.createElement("div");
+                price.className = "price";
 
-        const monthlyPrice = document.createElement("p");
-        monthlyPrice.textContent =
-          currentPricing === "monthly" ? plan.monthlyPrice : plan.annualPrice;
+                const monthlyPrice = document.createElement("p");
+                monthlyPrice.textContent =
+                    currentPricing === "monthly" ? plan.monthlyPrice : plan.annualPrice;
 
-        const month = document.createElement("small");
-        month.textContent = " / month";
+                const month = document.createElement("small");
+                month.textContent = " / month";
 
 
-        const extractNumber = (price) => {
-          return parseFloat(price.replace(/[^0-9.-]+/g, ""));
+                const extractNumber = (price) => {
+                    return parseFloat(price.replace(/[^0-9.-]+/g, ""));
                 };
-        
-        const annuallyBill = extractNumber(plan.annualPrice) * 12;
-        const annuallyBills = Math.round(annuallyBill);
-        const bill = document.createElement("p");
-        bill.className = `bill ${plan.popular ? "popular-bill" : ""}`;
-        bill.textContent =
-          currentPricing === "monthly" ? "Billed monthly" : "Billed annually " + "($" + annuallyBills + ")";
-        
-        // append children
-        price.appendChild(monthlyPrice);
-        price.appendChild(month);
 
-        // append children
-        textPairTwo.appendChild(price);
-        textPairTwo.appendChild(bill);  
+                const annuallyBill = extractNumber(plan.annualPrice) * 12;
+                const annuallyBills = Math.round(annuallyBill);
+                const bill = document.createElement("p");
+                bill.className = `bill ${plan.popular ? "popular-bill" : ""}`;
+                bill.textContent =
+                    currentPricing === "monthly" ? "Billed monthly" : "Billed annually " + "($" + annuallyBills + ")";
 
-        const featureList = document.createElement("ul");
-        featureList.className = "row-one";
-        featureList.role = "list";
+                // append children
+                price.appendChild(monthlyPrice);
+                price.appendChild(month);
 
-        plan.features.forEach((feature) => {
-          const listItem = document.createElement("li");
-          const textBody = document.createElement("div");
-          textBody.className = "text-body";
+                // append children
+                textPairTwo.appendChild(price);
+                textPairTwo.appendChild(bill);
 
-          const textPair = document.createElement("div");
-          textPair.className = "text-pair";
+                const featureList = document.createElement("ul");
+                featureList.className = "row-one";
+                featureList.role = "list";
 
-          // Create the SVG element and its child elements
-          const svg = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "svg"
-          );
-          svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-          svg.setAttribute("viewBox", "0 0 24 24");
-          svg.setAttribute("fill", "currentColor");
-          svg.setAttribute("width", "24"); // Set the width of the SVG
-          svg.setAttribute("height", "24"); // Set the height of the SVG
+                plan.features.forEach((feature) => {
+                    const listItem = document.createElement("li");
+                    const textBody = document.createElement("div");
+                    textBody.className = "text-body";
 
-          const path = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "path"
-          );
-          path.setAttribute(
-            "d",
-            "M9.9997 15.1709L19.1921 5.97852L20.6063 7.39273L9.9997 17.9993L3.63574 11.6354L5.04996 10.2212L9.9997 15.1709Z"
-          );
+                    const textPair = document.createElement("div");
+                    textPair.className = "text-pair";
 
-          svg.appendChild(path); // Append the path to the SVG
-          listItem.appendChild(svg); // Append the SVG to the list item
+                    // Create the SVG element and its child elements
+                    const svg = document.createElementNS(
+                        "http://www.w3.org/2000/svg",
+                        "svg"
+                    );
+                    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+                    svg.setAttribute("viewBox", "0 0 24 24");
+                    svg.setAttribute("fill", "currentColor");
+                    svg.setAttribute("width", "24"); // Set the width of the SVG
+                    svg.setAttribute("height", "24"); // Set the height of the SVG
 
-          const text = document.createElement("p");
-          text.textContent = feature;
+                    const path = document.createElementNS(
+                        "http://www.w3.org/2000/svg",
+                        "path"
+                    );
+                    path.setAttribute(
+                        "d",
+                        "M9.9997 15.1709L19.1921 5.97852L20.6063 7.39273L9.9997 17.9993L3.63574 11.6354L5.04996 10.2212L9.9997 15.1709Z"
+                    );
 
-          textPair.appendChild(text);
-          textBody.appendChild(textPair);
-          listItem.appendChild(textBody);
+                    svg.appendChild(path); // Append the path to the SVG
+                    listItem.appendChild(svg); // Append the SVG to the list item
 
-          featureList.appendChild(listItem);
-        });
+                    const text = document.createElement("p");
+                    text.textContent = feature;
 
-        const button = document.createElement("button");
-        button.className = "btn-buy-now";
+                    textPair.appendChild(text);
+                    textBody.appendChild(textPair);
+                    listItem.appendChild(textBody);
 
-        const buttonText = document.createElement("span");
-        buttonText.textContent = "Buy now";
+                    featureList.appendChild(listItem);
+                });
 
-        button.appendChild(buttonText);
+                const button = document.createElement("button");
+                button.className = "btn-buy-now";
 
-        pricingCard.appendChild(highlight);
-        pricingCard.appendChild(textPairOne);
-        pricingCard.appendChild(textPairTwo);
-        pricingCard.appendChild(featureList);
+                const buttonText = document.createElement("span");
+                buttonText.textContent = "Buy now";
 
-        if (plan.popular) {
-          const footer = document.createElement("div");
-          footer.className = "footer";
-          footer.appendChild(button);
-          pricingCard.appendChild(footer);
-        } else {
-          pricingCard.appendChild(button);
-        }
+                button.appendChild(buttonText);
 
-        return pricingCard;
-      };
+                pricingCard.appendChild(highlight);
+                pricingCard.appendChild(textPairOne);
+                pricingCard.appendChild(textPairTwo);
+                pricingCard.appendChild(featureList);
 
-      const createPlanContainer = (planName) => {
-        const planContainer = document.createElement("div");
-        planContainer.className = `${planName
-          .replace(/\s+/g, "-")
-          .toLowerCase()}`;
-        return planContainer;
-      };
+                if (plan.popular) {
+                    const footer = document.createElement("div");
+                    footer.className = "footer";
+                    footer.appendChild(button);
+                    pricingCard.appendChild(footer);
+                } else {
+                    pricingCard.appendChild(button);
+                }
 
-      const renderPlans = () => {
-        subscriptionContainer.innerHTML = ""; 
+                return pricingCard;
+            };
 
-        pricingData.forEach((plan) => {
-          const planContainer = createPlanContainer(plan.plan);
-          const pricingCard = createCards(plan);
-          planContainer.appendChild(pricingCard);
-          subscriptionContainer.appendChild(planContainer);
-        });
-      };
+            const createPlanContainer = (planName) => {
+                const planContainer = document.createElement("div");
+                planContainer.className = `${planName
+                    .replace(/\s+/g, "-")
+                    .toLowerCase()}`;
+                return planContainer;
+            };
 
-     const monthlyBtn =  document
-        .getElementById("toggle-monthly");
-        monthlyBtn.addEventListener("click", () => {
-          currentPricing = "monthly";
-          renderPlans();
-          annuallyBtn.classList.remove("active");
-          monthlyBtn.classList.add("active");
-        });
+            const renderPlans = () => {
+                subscriptionContainer.innerHTML = "";
 
- const annuallyBtn =     document
-        .getElementById("toggle-annually");
-        annuallyBtn.addEventListener("click", () => {
-          currentPricing = "annually";
-          renderPlans();
-          annuallyBtn.classList.add("active");
-          monthlyBtn.classList.remove("active");
-        });
+                pricingData.forEach((plan) => {
+                    const planContainer = createPlanContainer(plan.plan);
+                    const pricingCard = createCards(plan);
+                    planContainer.appendChild(pricingCard);
+                    subscriptionContainer.appendChild(planContainer);
+                });
+            };
 
-      renderPlans();
+            const monthlyBtn = document
+                .getElementById("toggle-monthly");
+            monthlyBtn.addEventListener("click", () => {
+                currentPricing = "monthly";
+                renderPlans();
+                annuallyBtn.classList.remove("active");
+                monthlyBtn.classList.add("active");
+            });
 
-        const updateBillText = () => {
-          const windowWidth = window.innerWidth;
-          const billElement = document.querySelector('.popular-bill');
-          if (windowWidth <= 500) {
-              billElement.textContent = "Price in USD";
-            } else {
-              billElement.textContent = currentPricing === 'monthly' ? 'Billed monthly' : 'Billed annually';
-            }
-        };
-      
-        updateBillText();
-      
-        window.addEventListener('resize', updateBillText);    
-      })
-  )
-  .catch(error => {
-    console.error('Error:', error);
-    const errorDiv = document.createElement('div');
-    errorDiv.classList.add('error');
-    errorDiv.textContent = `Error: ${error.message} Error fetching data. Please try again later.`;
-    teamContent.appendChild(errorDiv);
-});
+            const annuallyBtn = document
+                .getElementById("toggle-annually");
+            annuallyBtn.addEventListener("click", () => {
+                currentPricing = "annually";
+                renderPlans();
+                annuallyBtn.classList.add("active");
+                monthlyBtn.classList.remove("active");
+            });
+
+            renderPlans();
+
+            const updateBillText = () => {
+                const windowWidth = window.innerWidth;
+                const billElement = document.querySelector('.popular-bill');
+                if (windowWidth <= 500) {
+                    billElement.textContent = "Price in USD";
+                } else {
+                    billElement.textContent = currentPricing === 'monthly' ? 'Billed monthly' : 'Billed annually';
+                }
+            };
+
+            updateBillText();
+
+            window.addEventListener('resize', updateBillText);
+        })
+    )
+    .catch(error => {
+        console.error('Error:', error);
+        const errorDiv = document.createElement('div');
+        errorDiv.classList.add('error');
+        errorDiv.textContent = `Error: ${error.message} Error fetching data. Please try again later.`;
+        teamContent.appendChild(errorDiv);
+    });
